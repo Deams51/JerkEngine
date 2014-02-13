17cbe3b4e7b5e4361aa979929b811007941cfd25
Anders <anders@art-studio.se>
2014-02-13 01:32:01 +0100

    Created the velocity buffer and renders it to screen
    Created the basic velocity buffer and render it to screen. The next step
    is to render it to a texture and then read from that texture when
    calculating the blurring. I have to find a good sampling algorithm for
    the blurring as well. I need to look into deltaTime from rendering calls
    as well since the velocity buffer produces various results depending on
    the frame rate.

---
+++ Assets/Scripts/Graphics/ObjectRenderScript.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ObjectRenderScript : MonoBehaviour
+{
+    private Matrix4x4 _previousObject2World;
+
+    void Start()
+    {
+        PostProcessScript camera = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<PostProcessScript>();
+        camera.AddToRenderList(this);
+    }
+
+    void OnRenderObject()
+    {
+        renderer.material.SetMatrix("_PrevObject2World", _previousObject2World);
+    }
+
+    public void OnPostRenderUpdate()
+    {
+        _previousObject2World = transform.renderer.localToWorldMatrix;
+    }
+}
