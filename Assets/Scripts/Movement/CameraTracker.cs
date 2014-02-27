92e418e232d0e5790e556a11d62ee10ce0cc593f
Anders <anders@art-studio.se>
2014-02-27 22:57:21 +0100

    Added camera tracking and longer track
    What the summary said.

---
+++ Assets/Scripts/Movement/CameraTracker.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraTracker : MonoBehaviour 
+{
+    public GameObject TrackingObject;
+    public float TrackingAmount = 5.5f;
+
+    private Transform _trackingObject;
+    private Vector3 _relCameraPosition;
+
+    void Awake()
+    {
+        _trackingObject = TrackingObject.transform;
+        _relCameraPosition = _trackingObject.position - this.transform.position;
+    }
+
+    void FixedUpdate()
+    {
+        transform.position = Vector3.Lerp(transform.position, _trackingObject.position - _relCameraPosition, TrackingAmount * Time.deltaTime);
+        transform.LookAt(_trackingObject);
+    }
+
+
+}
